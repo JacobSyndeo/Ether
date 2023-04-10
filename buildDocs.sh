15f74131a17391ecc60c5f747d6fa4cf0ed32aed
Jacob Pritchett <jacobpritchett@me.com>
2023-04-09 20:52:34 -0600

    Fix base path for GitHub pages documentation 2

---
+++ buildDocs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+swift package --allow-writing-to-directory ./docs \
+              generate-documentation --target Ether \
+              --disable-indexing \
+              --transform-for-static-hosting \
+              --hosting-base-path / \
+              --output-path ./docs
